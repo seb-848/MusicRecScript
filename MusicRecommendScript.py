d59da650917b82e1416b6b68a2bd9922a536deec
seb-848 <sebastianecheverry848@gmail.com>
2025-09-25 02:16:27 -0400

    flask update

--- MusicRecommendScript.py
+++ MusicRecommendScript.py
@@ -1,35 +1,36 @@
-import os
-import requests
-
+from flask import Flask, request, jsonify
+import requests, os
 from dotenv import load_dotenv
 
-load_dotenv()
-
+MusicRecommendScript = Flask(__name__)
 
 API_KEY = os.getenv("API_KEY")
 BASE_URL = 'http://ws.audioscrobbler.com/2.0'
 
-artist = input("Enter an artist name: ")
+@MusicRecommendScript.route('/similar')
+def  similar():
+    artist = request.args.get("artist")
+    if not artist:
+        return jsonify({"error": "Provide artist name"}), 400
 
-params = {
-    "method": "artist.getSimilar",
-    "artist": artist,
-    "api_key": API_KEY,
-    "format": "json",
-    "limit": 5
-}
+    # Call the Last.fm API
+    r = requests.get(BASE_URL,
+                     params={
+                        "method": "artist.getSimilar",
+                        "artist": artist,
+                        "api_key": API_KEY,
+                        "format": "json",
+                        "limit": 5
+                     })
+    
+    if r.status_code != 200:
+        return jsonify({"Error": "Failed to fetch data"}), 500
+    
+    data = r.json()
 
-# Send http GET request
-response = requests.get(BASE_URL, params = params)
-
-# Convert JSON -> Python dict
-data = response.json()
-
-if response.status_code != 200:
-    print("Error:", response.status_code)
-elif "similarartists" not in data:
-    print("No results. Check spelling or try another artist.")
-else:
-    print("\nArtists similar to ", artist, ": ")
-    for a in data['similarartists']['artist']:
-        print("",a['name'])
+    try:
+        artists = [a["name"] for a in data["similarartists"]["artist"]]
+    except KeyError:
+        return jsonify({"Error": "Artist not foun"}), 404
+    
+    return jsonify({"artist": artist, "similar_artists": artists})
